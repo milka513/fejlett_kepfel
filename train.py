76460e1720630a4c4806d219bf68517c78b685e5
Heinc Emília <h657654@stud.u-szeged.hu>
2019-11-21 22:54:44 +0100

    added ppt and commented code

--- train.py
+++ train.py
@@ -5,12 +5,9 @@ from sklearn import svm
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
-import ProcessImage as image
-from sklearn.neighbors import KNeighborsClassifier
 class train(object):
 
     def __init__(self):
@@ -25,27 +22,24 @@ class train(object):
         #self.clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
         #self.clf.fit(X, y)
 
-        #steps = [('pca', PCA()), ('clf', SVC(kernel='rbf'))]
-        steps = [('pca', PCA()), ('clf', SVC(kernel='poly'))]
-        #steps = [('pca', PCA()), ('clf', KNeighborsClassifier(n_neighbors=15))]
+        steps = [('pca', PCA()), ('clf', SVC(kernel='rbf'))]
         pipe = Pipeline(steps)
-        pca__n_components=[7]
+        pca__n_components=[6]
         n_splits = 6
         cv = StratifiedKFold(n_splits=n_splits, shuffle=True)
         clf__gamma = np.logspace(-4, -2, 3)  # [.0001, .001, .01]
         clf__C = np.logspace(0, 2, 3)  # [1, 10, 100]
         grid_params = dict(pca__n_components=pca__n_components,
                            clf__gamma=clf__gamma,
-                            clf__C=clf__C)
-        #grid_params=dict(n_neighbors=15)
+                           clf__C=clf__C)
         self.grid = GridSearchCV(pipe, grid_params, cv=cv, refit=True, n_jobs=-1, scoring='f1_micro')
         self.grid.fit(X, y)
-        with open('out/clf_poly.pkl', 'wb') as f:
+        with open('out/clf.pkl', 'wb') as f:
             f.flush()
             pickle.dump(self.grid, f)
 
     def load(self):
-        filename='out/clf_poly.pkl'
+        filename='out/clf.pkl'
         with open(filename, 'rb') as f:
             self.grid=pickle.load(f)
 
@@ -57,8 +51,3 @@ class train(object):
         #std = list.std(axis=0)
         #list = (list - mean) / std
         return self.grid.predict(list)
-
-    def test(self):
-        im = image.ProcessImages('test')
-        X, Y = im.make_all()
-        return self.grid.score(X, Y)
